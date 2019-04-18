6abaa512eb97ca906b3965f0be7aa0665eb3a55b
jvdwyer <jvdwyer1994@gmail.com>
2019-04-18 17:07:17 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def calcAngle(time):
+    big_hand = int(time.partition(':')[0])
+    little_hand = int(time.partition(':')[2])
+    if big_hand >= 12:
+        big_hand -= 12
+    big_hand_angle = float((big_hand)*30)
+    for i in range(little_hand):
+        big_hand_angle += .5
+    little_hand_angle = 6 * little_hand
+    return 360 - big_hand_angle - little_hand_angle
+
+print(calcAngle('3:10'))
